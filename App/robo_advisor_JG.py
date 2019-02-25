e738f337e8fe011ac5fe56e93fa0bd519c092d97
jag378 <46541182+jag378@users.noreply.github.com>
2019-02-25 12:53:26 -0500

    imported packages and Hello World

---
+++ App/robo_advisor_JG.py
@@ -0,0 +1,12 @@
+#Initial Commit
+
+import json
+import os
+import requests
+import csv
+
+
+#pip install python-dotenv through the command prompt
+from dotenv import load_dotenv
+
+print("Hello World")
